93de0417579fc274eb711d55191fdd66638c59cf
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-11-13 18:08:51 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -2,26 +2,26 @@
 #set -o allexport; source .env; set +o allexport;
 
 mkdir -p ./postgres-data
-# chown -R 1000:1000 ./postgres-data
+chown -R 1001:1001 ./postgres-data
 
 mkdir -p ./posthog
-# chown -R 1000:1000 ./posthog
+chown -R 1001:1001 ./posthog
 
 mkdir -p ./zookeeper-datalog
-# chown -R 1000:1000 ./zookeeper-datalog
+chown -R 1001:1001 ./zookeeper-datalog
 
 mkdir -p ./zookeeper-data
-# chown -R 1000:1000 ./zookeeper-data
+chown -R 1001:1001 ./zookeeper-data
 
 mkdir -p ./zookeeper-logs
-# chown -R 1000:1000 ./zookeeper-logs
+chown -R 1001:1001 ./zookeeper-logs
 
 mkdir -p ./compose
-# chown -R 1000:1000 ./compose
+chown -R 1001:1001 ./compose
 
 mkdir -p ./object_storage
-# chown -R 1000:1000 ./object_storage
+chown -R 1001:1001 ./object_storage
 
 mkdir -p ./clickhouse-data
-# chown -R 1000:1000 ./clickhouse-data
+chown -R 1001:1001 ./clickhouse-data
 
