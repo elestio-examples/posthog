6a3b760afdeeea92af5fbd3ddb74ea9ab3a7ad50
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-11 17:53:15 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,5 +1,5 @@
 #set env vars
-#set -o allexport; source .env; set +o allexport;
+set -o allexport; source .env; set +o allexport;
 
 
 POSTHOG_SECRET=$(head -c 28 /dev/urandom | sha224sum -b | head -c 56)
@@ -9,3 +9,13 @@ cat << EOT >> ./.env
 POSTHOG_SECRET=${POSTHOG_SECRET}
 EOT
 
+cat << EOT >> ./Caddyfile
+
+{
+    acme_ca https://acme-staging-v02.api.letsencrypt.org/directory
+}
+    ${DOMAIN}, :80, :443 {
+    reverse_proxy http://web:8000
+}
+
+EOT
